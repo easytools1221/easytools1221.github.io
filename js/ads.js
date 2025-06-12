80f69870e5bcde27988a2e849505847f55783280
Jin-Wei Chang <jinwei.chang1221@gmail.com>
2025-06-12 07:58:12 +0100

    Update Home page ads insertion

---
+++ js/ads.js
@@ -0,0 +1,43 @@
+
+
+function insert_ads_card_list(){
+    const card_list_container = document.getElementById("tools_list")    
+    const cards = card_list_container.querySelectorAll(".col-md-6.col-lg-4")
+    const ads_insert_interval = 4
+
+    const adClient = 'ca-pub-7255560333193386';
+    const adSlot = '1706287114';
+    const adFormat = 'fluid'; 
+    const adLayoutKey = '-gw-3+1f-3d+2z';
+    
+    let ads_count = 0
+
+    cards.forEach((card, index) => {
+        if ((index + 1) % ads_insert_interval === 0) {
+            const card_node = card.cloneNode(true) 
+            const card_body = card_node.querySelector(".card-body")
+
+            card_body.innerHTML = ""
+
+            const insElement = document.createElement('ins');
+            insElement.className = 'adsbygoogle';
+            insElement.style.display = 'block';
+            insElement.setAttribute('data-ad-client', adClient);
+            insElement.setAttribute('data-ad-slot', adSlot);
+            insElement.setAttribute('data-ad-format', adFormat);
+            insElement.setAttribute('data-ad-layout-key', adLayoutKey);
+
+            card_body.appendChild(insElement);
+
+            // 將廣告容器插入到當前卡片之後
+            card.parentNode.insertBefore(card_node, card.nextSibling);
+
+            ads_count++; // 記錄插入的廣告數量
+        }
+    })
+
+    if(ads_count > 0){
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+
+}
